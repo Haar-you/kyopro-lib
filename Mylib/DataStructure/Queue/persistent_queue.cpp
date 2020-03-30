9d6b81236e00983190a1045949de831170cfa97c
Haar-you <zemlya.pole@gmail.com>
2020-03-31 08:22:27 +0900

    queue

---
+++ Mylib/DataStructure/Queue/persistent_queue.cpp
@@ -0,0 +1,82 @@
+#pragma once
+#include <array>
+
+/**
+ * @attention time complexity of push(value) and pop() : O(MAX_SIZE_2)
+ */
+template <typename T>
+class PersistentQueue{
+  constexpr static int MAX_SIZE_2 = 20; // size <= 2 ^ MAX_SIZE_2
+  
+  struct node{
+    T value;
+    std::array<node*, MAX_SIZE_2> ancestors;
+    int depth = 0;
+  };
+
+  node *front_node = nullptr, *back_node = nullptr;
+
+  PersistentQueue(node* front_node, node* back_node): front_node(front_node), back_node(back_node){}
+  
+  
+public:
+  PersistentQueue() {}
+  PersistentQueue(const T &value){
+    node *t = new node();
+    t->value = value;
+    back_node = front_node = t;
+  }
+  
+  PersistentQueue push(const T &value){
+    node *t = new node();
+
+    t->value = value;
+    
+    t->ancestors[0] = back_node;
+    for(int i = 1; i < MAX_SIZE_2; ++i){
+      node *s = t->ancestors[i-1];
+      if(s) t->ancestors[i] = s->ancestors[i-1];
+      else break;
+    }
+
+    t->depth = back_node ? back_node->depth + 1 : 0;
+    
+    return PersistentQueue(front_node ? front_node : t, t);
+  }
+
+  PersistentQueue pop(){
+    if(back_node->depth == front_node->depth){
+      return PersistentQueue(nullptr, nullptr);
+    }
+    
+    int d = back_node->depth - front_node->depth - 1;
+
+    node *t = back_node;
+
+    for(int i = MAX_SIZE_2 - 1; i >= 0; --i){
+      if(d >= (1 << i)){
+        d -= (1 << i);
+        t = t->ancestors[i];
+      }
+      if(d == 0) break;
+    }
+    
+    return PersistentQueue(t, back_node);
+  }
+
+  T front() const {
+    return front_node->value;
+  }
+
+  T back() const {
+    return back_node->value;
+  }
+
+  bool empty() const {
+    return not front_node;
+  }
+
+  int size() const {
+    return front_node ? 0 : back_node->depth - front_node->depth + 1;
+  }
+};
---
+++ test/yosupo-judge/persistent_queue/main.test.cpp
@@ -0,0 +1,33 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/persistent_queue"
+
+#include <iostream>
+#include <vector>
+#include "Mylib/DataStructure/Queue/persistent_queue.cpp"
+
+int main(){
+  int Q; std::cin >> Q;
+
+  std::vector<PersistentQueue<int>> S;
+
+  while(Q--){
+    int type; std::cin >> type;
+
+    if(type == 0){
+      int t, x; std::cin >> t >> x;
+      if(t == -1){
+        PersistentQueue<int> a(x);
+        S.push_back(a);
+      }else{
+        auto res = S[t].push(x);
+        S.push_back(res);
+      }
+    }else{
+      int t; std::cin >> t;
+      std::cout << S[t].front() << std::endl;
+      auto res = S[t].pop();
+      S.push_back(res);
+    }
+  }
+  
+  return 0;
+}
