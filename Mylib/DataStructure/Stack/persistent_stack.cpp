df82b4845cc58e442cf55a492db5978fd8aee0e2
Haar-you <zemlya.pole@gmail.com>
2020-03-31 09:40:40 +0900

    persistent stack

---
+++ Mylib/DataStructure/Stack/persistent_stack.cpp
@@ -0,0 +1,34 @@
+#pragma once
+
+template <typename T> struct PersistentStack{
+protected:
+  struct node{
+    T value;
+    node *next = nullptr;
+  };
+
+  node *root;
+
+  PersistentStack(node *root): root(root){}
+
+public:
+  PersistentStack(): root(nullptr){}
+  
+  bool empty() const {
+    return root == nullptr;
+  }
+  
+  const T& top() const {
+    return root->value;
+  }
+
+  PersistentStack push(const T &value) const {
+    node *t = new node({value, root});
+    return PersistentStack(t);
+  }
+
+  PersistentStack pop() const {
+    node *t = root->next;
+    return PersistentStack(t);
+  }
+};
