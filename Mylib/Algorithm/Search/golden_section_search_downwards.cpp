c676076e681638b143da5e1afcdab12b3bb0f75a
Haar-you <zemlya.pole@gmail.com>
2020-04-15 00:08:08 +0900

    fix include

--- Mylib/Algorithm/Search/golden_section_search_downwards.cpp
+++ Mylib/Algorithm/Search/golden_section_search_downwards.cpp
@@ -1,12 +1,13 @@
 #pragma once
 #include <functional>
+#include <cmath>
 
 /**
  * @title 下に凸の黄金分割探索
  */
 template <typename T = double, typename Func = std::function<T(T)>>
 T golden_section_search_downwards(T lb, T ub, const Func &f, int LOOP_COUNT = 100){
-  constexpr T phi = (1.0 + sqrt(5)) / 2;
+  constexpr T phi = (1.0 + std::sqrt(5)) / 2;
   
   T t1, t2;
 
