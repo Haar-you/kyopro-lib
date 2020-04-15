860d209c5160df7c4dd06d8a2896dc743f3992d0
Haar-you <zemlya.pole@gmail.com>
2020-04-15 20:46:25 +0900

    add quadratic equation

---
+++ Mylib/Math/quadratic_equation.cpp
@@ -0,0 +1,15 @@
+#pragma once
+#include <cmath>
+#include <vector>
+
+/**
+ * @title 二次方程式の実数解
+ */
+std::vector<double> quadratic_equation(double a, double b, double c){
+  double d = b*b - 4*a*c;
+  if(d < 0) return {};
+  
+  double x1 = (-b - std::sqrt(d)) / (2 * a);
+  double x2 = (-b + std::sqrt(d)) / (2 * a);
+  return {x1, x2};
+}
