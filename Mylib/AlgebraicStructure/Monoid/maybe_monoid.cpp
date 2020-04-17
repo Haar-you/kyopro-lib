1609c6048c4bfd963fd14f4a9d1c2312efb05f48
Haar-you <zemlya.pole@gmail.com>
2020-04-18 02:38:53 +0900

    new maybe_monoid

---
+++ Mylib/AlgebraicStructure/Monoid/maybe_monoid.cpp
@@ -0,0 +1,14 @@
+#pragma once
+#include <optional>
+
+template <typename Semigroup>
+struct MaybeMonoid{
+  using value_type = std::optional<typename Semigroup::value_type>;
+  
+  static value_type id(){return {};}
+  static value_type op(const value_type &a, const value_type &b){
+    if(not a) return b;
+    if(not b) return a;
+    return {Semigroup::op(*a, *b)};
+  }
+};
