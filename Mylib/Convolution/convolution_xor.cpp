1f0288631392e353be9d79330bea86a9f69fc1f4
Haar-you <zemlya.pole@gmail.com>
2020-04-30 14:59:26 +0900

    convolution xor

---
+++ Mylib/Convolution/convolution_xor.cpp
@@ -0,0 +1,50 @@
+#pragma once
+#include <vector>
+#include <cassert>
+
+/**
+ * @title 添字BitXor畳み込み
+ * @docs convolution_xor.md
+ */
+template <typename T>
+std::vector<T> convolution_xor(std::vector<T> f, std::vector<T> g){
+  const int n = f.size();
+  assert((int)f.size() == n and (int)g.size() == n and (n & (n-1)) == 0);
+
+  auto fwt =
+    [n](std::vector<T> f){
+      for(int i = 1; i < n; i <<= 1){
+        for(int j = 0; j < n; ++j){
+          if((j & i) == 0){
+            auto x = f[j], y = f[j | i];
+            f[j] = x + y;
+            f[j | i] = x - y;
+          }
+        }
+      }
+      return f;
+    };
+
+  auto ifwt =
+    [n](std::vector<T> f){
+      for(int i = 1; i < n; i <<= 1){
+        for(int j = 0; j < n; ++j){
+          if((j & i) == 0){
+            auto x = f[j], y = f[j | i];
+            f[j] = (x + y) / 2;
+            f[j | i] = (x - y) / 2;
+          }
+        }
+      }
+      return f;
+    };
+
+  f = fwt(f);
+  g = fwt(g);
+
+  for(int i = 0; i < n; ++i) f[i] *= g[i];
+
+  f = ifwt(f);
+
+  return f;
+}
---
+++ Mylib/Convolution/convolution_xor.md
@@ -0,0 +1,11 @@
+## Operations
+
+## Requirements
+
+## Notes
+
+## Problems
+
+## References
+
+- [https://kazuma8128.hatenablog.com/entry/2018/05/31/144519](https://kazuma8128.hatenablog.com/entry/2018/05/31/144519)
