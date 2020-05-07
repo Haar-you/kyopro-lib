1b88695c48a582ab10e78c5061adaba64bfe718a
Haar-you <zemlya.pole@gmail.com>
2020-05-07 18:39:04 +0900

    rollbackable vector

---
+++ Mylib/DataStructure/Array/rollbackable_vector.cpp
@@ -0,0 +1,47 @@
+#pragma once
+#include <vector>
+
+/**
+ * @title Roll-back可能Vector
+ * @docs rollbackable_vector.md
+ */
+template <typename T>
+class RollbackableVector{
+  std::vector<T> data;
+  std::vector<std::vector<T>> stock;
+  std::vector<int> history;
+
+public:
+  void push_back(const T &value){
+    data.push_back(value);
+    stock.push_back({value});
+    history.push_back(-1);
+  }
+
+  void assign(int i, const T &value){
+    data[i] = value;
+    stock[i].push_back(value);
+    history.push_back(i);
+  }
+
+  void roll_back(){
+    const int i = history.back(); history.pop_back();
+
+    if(i == -1){
+      data.pop_back();
+      stock.pop_back();
+    }else{
+      stock[i].pop_back();
+      data[i] = stock[i].back();
+    }
+  }
+
+  auto cbegin() const {return data.cbegin();}
+  auto cend() const {return data.cend();}
+
+  int size() const {return data.size();}
+  bool empty() const {return data.empty();}
+
+  const T& back() const {return data.back();}
+  const T& front() const {return data.front();}
+};
---
+++ Mylib/DataStructure/Array/rollbackable_vector.md
@@ -0,0 +1,12 @@
+## Operations
+
+## Requirements
+
+## Notes
+
+## Problems
+
+- [ABC 165 F - LIS on Tree](https://atcoder.jp/contests/abc165/tasks/abc165_f)
+
+## References
+
