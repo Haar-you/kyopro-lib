68144df80548af1a96d0bf4ed52105d611129bf3
Haar-you <zemlya.pole@gmail.com>
2020-05-16 08:50:45 +0900

    formal power series

---
+++ Mylib/Convolution/formal_power_series.cpp
@@ -0,0 +1,141 @@
+#pragma once
+
+#include <functional>
+#include <vector>
+
+/**
+ * @title FormalPowerSeries
+ * @docs formal_power_series.md
+ */
+template <typename T>
+struct FPS{
+  static std::function<std::vector<T>(std::vector<T>, std::vector<T>)> convolve;
+
+  std::vector<T> data;
+
+  FPS(const std::vector<T> &data): data(data){}
+
+  int size() const {
+    return data.size();
+  }
+
+  const T& operator[](int i) const {
+    return data[i];
+  }
+
+  T& operator[](int i){
+    return data[i];
+  }
+
+  void resize(int n){
+    data.resize(n);
+  }
+  
+  auto operator+(const FPS &rhs) const {
+    std::vector<T> ret(data);
+    ret.resize(rhs.size());
+    for(int i = 0; i < (int)rhs.size(); ++i) ret[i] += rhs[i];
+    return FPS(ret);
+  }
+
+  auto operator-(const FPS &rhs) const {
+    std::vector<T> ret(data);
+    ret.resize(rhs.size());
+    for(int i = 0; i < (int)rhs.size(); ++i) ret[i] -= rhs[i];
+    return FPS(ret);
+  }
+
+  auto operator*(const FPS &rhs) const {
+    auto ret = convolve(data, rhs.data);
+    return FPS(ret);
+  }
+
+  auto operator*(T b) const {
+    std::vector<T> ret(data);
+    for(auto &x : ret) x *= b;
+    return FPS(ret);
+  }
+
+  auto differentiate() const {
+    const int n = data.size();
+    std::vector<T> ret(n-1);
+    for(int i = 0; i < n-1; ++i){
+      ret[i] = data[i+1] * (i + 1);
+    }
+    
+    return FPS(ret);
+  }
+
+  auto integrate() const {
+    const int n = data.size();
+    std::vector<T> ret(n+1);
+    for(int i = 0; i < n; ++i){
+      ret[i+1] = data[i] / (i + 1);
+    }
+
+    return FPS(ret);
+  }
+
+  auto inv() const {
+    assert(data[0] != 0);
+    const int n = data.size();
+    
+    int t = 1;
+    std::vector<T> ret = {data[0].inv()};
+    ret.reserve(n * 2);
+    
+    while(t <= n * 2){
+      std::vector<T> c(data.begin(), data.begin() + std::min(t, n));
+      c = convolve(c, convolve(ret, ret));
+
+      c.resize(t);
+      ret.resize(t);
+
+      for(int i = 0; i < t; ++i){
+        ret[i] = ret[i] * 2 - c[i];
+      }
+      
+      t <<= 1;
+    }
+    
+    ret.resize(n);
+
+    return FPS(ret);
+  }
+
+  auto log() const {
+    assert(data[0] == 1);
+    const int n = data.size();
+    auto ret = (differentiate() * inv()).integrate();
+    ret.resize(n);
+    return ret;
+  }
+
+  auto exp() const {
+    const int n = data.size();
+
+    int t = 1;
+    FPS b({1});
+
+    while(t <= n * 2){
+      t <<= 1;
+      auto temp = b.log();
+      temp.resize(t);
+
+      for(int i = 0; i < t; ++i) temp[i] = -temp[i];
+      temp[0] += 1;
+      for(int i = 0; i < std::min(t, n); ++i) temp[i] += data[i];
+      
+      b = b * temp;
+      b.resize(t);
+    }
+    
+    b.resize(n);
+
+    return b;
+  }
+};
+
+
+template <typename T>
+std::function<std::vector<T>(std::vector<T>, std::vector<T>)> FPS<T>::convolve;
