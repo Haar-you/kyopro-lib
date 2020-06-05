f401d7c7f55fac2fb60fdeeac0a6823c9362eb1e
Haar-you <zemlya.pole@gmail.com>
2020-06-06 00:48:58 +0900

    int128_t

--- Mylib/Misc/int128.cpp
+++ Mylib/Misc/int128.cpp
@@ -6,7 +6,9 @@
  */
 #ifdef __SIZEOF_INT128__
 using uint128_t = __uint128_t;
+using int128_t = __int128_t;
 #else
 #include <boost/multiprecision/cpp_int.hpp>
 using uint128_t = boost::multiprecision::uint128_t;
+using int128_t = boost::multiprecision::int128_t;
 #endif
