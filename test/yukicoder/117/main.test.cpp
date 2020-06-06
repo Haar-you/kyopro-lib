78ad2acda80a08152da9dbaf55c5dcb494a00a20
Haar-you <zemlya.pole@gmail.com>
2020-06-06 10:19:56 +0900

    regexで書き直し

--- test/yukicoder/117/main.test.cpp
+++ test/yukicoder/117/main.test.cpp
@@ -1,27 +1,38 @@
 #define PROBLEM "https://yukicoder.me/problems/no/117"
 
 #include <iostream>
+#include <string>
+#include <regex>
 
 #include "Mylib/Number/Mint/mint.cpp"
 #include "Mylib/Combinatorics/combinatorics.cpp"
+#include "Mylib/IO/input_tuples.cpp"
 
 using mint = ModInt<1000000007>;
 using C = Combinatorics<mint>;
 
 int main(){
+  std::cin.tie(0);
+  std::ios::sync_with_stdio(false);
+
   C::init(2000000);
   
-  int T; scanf("%d", &T);
+  int T; std::cin >> T;
 
-  while(T--){
-    char type;
-    int N, K;
-    scanf(" %c(%d,%d)", &type, &N, &K);
+  std::regex re(R"((.)\((.+),(.+)\))");
+  
+  for(auto [s] : input_tuples<std::string>(T)){
+    std::smatch m;
+    std::regex_match(s, m, re);
+
+    char type = m[1].str()[0];
+    int N = std::stoi(m[2].str());
+    int K = std::stoi(m[3].str());
 
     switch(type){
-    case 'C': printf("%d\n", (int)C::C(N, K)); break;
-    case 'P': printf("%d\n", (int)C::P(N, K)); break;
-    case 'H': printf("%d\n", (int)C::H(N, K)); break;
+    case 'C': std::cout << C::C(N, K) << "\n"; break;
+    case 'P': std::cout << C::P(N, K) << "\n"; break;
+    case 'H': std::cout << C::H(N, K) << "\n"; break;
     }
   }
 
