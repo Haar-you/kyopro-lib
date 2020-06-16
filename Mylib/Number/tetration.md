577b6f5ac0578dee0f96445be55f45c0a8d1deb1
Haar-you <zemlya.pole@gmail.com>
2020-06-16 23:16:56 +0900

    tetration

---
+++ Mylib/Number/tetration.md
@@ -0,0 +1,11 @@
+## Operations
+
+## Requirements
+
+## Notes
+
+## Problems
+
+## References
+
+- [https://kopricky.github.io/code/Computation_Advanced/tetration.html](https://kopricky.github.io/code/Computation_Advanced/tetration.html)
---
+++ test/yosupo-judge/tetration_mod/main.test.cpp
@@ -0,0 +1,17 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/tetration_mod"
+
+#include <iostream>
+
+#include "Mylib/IO/input_tuples.cpp"
+#include "Mylib/Number/tetration.cpp"
+
+int main(){
+  int T; std::cin >> T;
+
+  for(auto [A, B, M] : input_tuples<int, int, int>(T)){
+    auto ans = tetration(A, B, M);
+    std::cout << ans << "\n";
+  }
+
+  return 0;
+}
