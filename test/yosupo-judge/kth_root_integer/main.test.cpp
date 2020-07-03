b0bad712380412ebe3f7e2ead589f7bb6e40f71c
Haar <haar@haar-pc>
2020-07-04 05:12:56 +0900

    kth root integer

---
+++ test/yosupo-judge/kth_root_integer/main.test.cpp
@@ -0,0 +1,18 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/kth_root_integer"
+
+#include <iostream>
+#include "Mylib/IO/input_tuples.cpp"
+#include "Mylib/Number/kth_root_integer.cpp"
+
+int main(){
+  std::cin.tie(0);
+  std::ios::sync_with_stdio(false);
+  
+  int T; std::cin >> T;
+
+  for(auto [A, K] : input_tuples<uint64_t, uint64_t>(T)){
+    std::cout << kth_root(A, K) << "\n";
+  }
+
+  return 0;
+}
