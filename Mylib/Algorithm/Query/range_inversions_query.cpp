16461b30617cc9ac90e6396cfc95e46da008e733
Haar <haar@haar-pc>
2020-08-04 00:01:19 +0900

    [new] range inversions query

---
+++ Mylib/Algorithm/Query/range_inversions_query.cpp
@@ -0,0 +1,70 @@
+#pragma once
+
+#include <vector>
+#include <utility>
+#include <algorithm>
+
+#include "Mylib/DataStructure/FenwickTree/fenwick_tree_add.cpp"
+#include "Mylib/Algorithm/Mo/mo_algorithm.cpp"
+
+/**
+ * @title Range inversions query
+ * @docs range_inversions_query.md
+ */
+template <typename T>
+class RangeInversionsQuery{
+  std::vector<int> a;
+  int N;
+  std::vector<std::pair<int, int>> qs;
+
+public:
+  RangeInversionsQuery(std::vector<T> a_): N(a_.size()){
+    auto b = a_;
+    std::sort(b.begin(), b.end());
+    b.erase(std::unique(b.begin(), b.end()), b.end());
+
+    for(auto x : a_){
+      const int i = std::lower_bound(b.begin(), b.end(), x) - b.begin();
+      a.push_back(i);
+    }
+  }
+
+  void add(int l, int r){ // [l, r)
+    qs.emplace_back(l, r);
+  }
+
+  auto solve(){
+    const int Q = qs.size();
+    FenwickTreeAdd<int64_t> b(N);
+
+    int64_t t = 0;
+    std::vector<int64_t> ans(Q);
+
+    auto append =
+      [&](int i, int d){
+        if(d == -1) t += b.get(0, a[i]);
+        else t += b.get(a[i] + 1, N);
+        b.update(a[i], 1);
+      };
+
+    auto remove =
+      [&](int i, int d){
+        if(d == -1) t -= b.get(0, a[i]);
+        else t -= b.get(a[i] + 1, N);
+        b.update(a[i], -1);
+      };
+
+    auto query = [&](int i){ans[i] = t;};
+
+    auto mo = make_mo(N, Q, append, remove, query);
+
+    for(auto [l, r] : qs){
+      mo.add(l, r);
+    }
+
+    mo.build();
+    mo.run();
+
+    return ans;
+  }
+};
---
+++ test/yosupo-judge/static_range_inversions_query/main.test.cpp
@@ -0,0 +1,30 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/static_range_inversions_query"
+
+#include <iostream>
+#include <vector>
+
+#include "Mylib/IO/input_vector.cpp"
+#include "Mylib/IO/input_tuples.cpp"
+#include "Mylib/Algorithm/Query/range_inversions_query.cpp"
+
+int main(){
+  std::cin.tie(0);
+  std::ios::sync_with_stdio(false);
+  
+  int N, Q; std::cin >> N >> Q;
+  auto A = input_vector<int>(N);
+
+  RangeInversionsQuery riq(A);
+
+  for(auto [l, r] : input_tuples<int, int>(Q)){
+    riq.add(l, r);
+  }
+
+  auto ans = riq.solve();
+  
+  for(auto x : ans){
+    std::cout << x << "\n";
+  }
+  
+  return 0;
+}
