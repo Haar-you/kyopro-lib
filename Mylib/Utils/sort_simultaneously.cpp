5aa47cd91fa58a6fde8db9eba2da8c7783113999
Haar-you <zemlya.pole@gmail.com>
2020-08-07 20:09:17 +0900

    [modify] sort_simultaneously.cpp

--- Mylib/Utils/sort_simultaneously.cpp
+++ Mylib/Utils/sort_simultaneously.cpp
@@ -3,6 +3,7 @@
 #include <utility>
 #include <algorithm>
 #include <numeric>
+#include <initializer_list>
 
 /**
  * @docs sort_simultaneously.md
@@ -14,19 +15,11 @@ void sort_with_ord(const std::vector<int> &ord, int N, std::vector<T> &a){
   std::swap(temp, a);
 }
 
-template <typename T, typename ...Args>
-void sort_with_ord(const std::vector<int> &ord, int N, std::vector<T> &a, Args&... args){
-  std::vector<T> temp(N);
-  for(int i = 0; i < N; ++i) temp[i] = a[ord[i]];
-  std::swap(temp, a);
-  sort_with_ord(ord, N, args...);
-}
-
 template <typename Compare, typename ...Args>
-void sort_simultaneously(const Compare &compare, int N, Args&... args){
+void sort_simultaneously(const Compare &compare, int N, std::vector<Args> &... args){
   std::vector<int> ord(N);
   std::iota(ord.begin(), ord.end(), 0);
   std::sort(ord.begin(), ord.end(), compare);
 
-  sort_with_ord(ord, N, args...);
+  (void)std::initializer_list<int>{(sort_with_ord(ord, N, args), 0)...};
 }
