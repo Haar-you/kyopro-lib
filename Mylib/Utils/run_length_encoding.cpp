0168e79a58d0c4f8a6bd7132920b245a6341e38c
Haar-you <zemlya.pole@gmail.com>
2020-08-07 20:11:20 +0900

    [modify] run_length_encoding.cpp

--- Mylib/Utils/run_length_encoding.cpp
+++ Mylib/Utils/run_length_encoding.cpp
@@ -11,9 +11,9 @@ auto run_length_encoding(const Container &v){
   std::vector<std::pair<T,int64_t>> ret;
 
   for(auto &x : v){
-    if(ret.empty()) ret.push_back({x,1});
+    if(ret.empty()) ret.emplace_back(x, 1);
     else if(ret.back().first == x) ++ret.back().second;
-    else ret.push_back({x,1});
+    else ret.emplace_back(x, 1);
   }
 
   return ret;
