9fef51112c26427bee0a59c446695d5a1ecd6c95
Haar-you <zemlya.pole@gmail.com>
2020-08-07 21:01:02 +0900

    [new] zip.cpp unzip.cpp

---
+++ Mylib/Misc/zip.cpp
@@ -0,0 +1,21 @@
+#pragma once
+#include <vector>
+#include <tuple>
+#include <algorithm>
+
+/**
+ * @title Zip function
+ * @docs zip.md
+ */
+template <typename ... Args>
+auto zip(std::vector<Args> ... a){
+  const int N = std::min({a.size() ...});
+  std::vector<std::tuple<Args...>> ret(N);
+
+  for(int i = 0; i < N; ++i){
+    ret[i] = std::make_tuple(a[i] ...);
+  }
+
+  return ret;
+}
+
