0809269888d74c2e5857925d10732782618937f2
Haar <haar@haar-pc>
2020-08-12 08:56:22 +0900

    [new] run.rb

---
+++ script/run.rb
@@ -0,0 +1,67 @@
+#!/usr/bin/env ruby
+
+require "optparse"
+require "open3"
+require "fileutils"
+require "pathname"
+
+opts = OptionParser.new
+
+$exec_only = false
+$debug = true
+$cpp_version = "c++1z"
+$boost = nil
+
+opts.on("-e", "--exec-only"){
+  $exec_only = true
+}
+
+opts.on("--no-debug"){
+  $debug = false
+}
+
+opts.on("--cpp VERSION"){|version|
+  $cpp_version = version
+}
+
+$mylib = Pathname.new(__dir__).parent
+
+opts.parse!(ARGV)
+source_file, input_file = ARGV
+
+if not $exec_only
+  puts "\e[33;1m compiling ... \e[m"
+
+  cmd = [
+    "g++",
+    "#{source_file}",
+    "-O2",
+    "-std=#{$cpp_version}",
+    "-Wall",
+    "-Wextra",
+    "-I#{$mylib}",
+    $boost.nil? ? "" : "-I#{$boost}",
+    $debug.nil? ? "" : "-DDEBUG",
+    "-D_GLIBCXX_DEBUG",
+    "-g",
+    "-o a.out"
+  ]
+
+  puts cmd.join(" ")
+
+  o, e, s = Open3.capture3(cmd.join(" "))
+
+  print o
+  print e
+
+  exit unless s.success?
+  
+  puts "\e[33;1m compiled \e[m"
+end
+
+
+if input_file.nil?
+  system("./a.out")
+else
+  system("./a.out < #{input_file}")
+end
