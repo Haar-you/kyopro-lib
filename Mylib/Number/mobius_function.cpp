2f307940f8f6146a7e3480d9049f7ad5b959838d
Haar <haar@haar-pc>
2020-08-12 08:07:34 +0900

    [modify] mobius function

--- Mylib/Number/mobius_function.cpp
+++ Mylib/Number/mobius_function.cpp
@@ -1,26 +1,26 @@
 #pragma once
 #include <vector>
-#include "Mylib/Number/Prime/eratosthenes_sieve.cpp"
 
 /**
  * @title Möbius function
  * @docs mobius_function.md
  */
-std::vector<int> mobius_function(int n){
+template <typename Checker>
+std::vector<int> mobius_function(int n, Checker is_prime){
   std::vector<int> ret(n+1);
   std::vector<int> ps;
 
   ret[1] = 1;
 
   for(int i = 2; i <= n; ++i){
-    if(eratosthenes_sieve::is_prime[i]){
+    if(is_prime(i)){
       ps.push_back(i);
       ret[i] = -1;
     }
     for(auto &j : ps){
-      if(i*j > n) break;
-      if(i%j == 0) ret[i*j] = 0;
-      else ret[i*j] = ret[i] * ret[j];
+      if(i * j > n) break;
+      if(i % j == 0) ret[i * j] = 0;
+      else ret[i * j] = ret[i] * ret[j];
     }
   }
   
