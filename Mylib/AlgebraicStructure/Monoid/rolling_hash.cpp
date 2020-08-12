acc369d65c76c88f065ebc99c62445c52d16a405
Haar <haar@haar-pc>
2020-08-13 07:39:18 +0900

    [new] rolling hash monoid

---
+++ Mylib/AlgebraicStructure/Monoid/rolling_hash.cpp
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <array>
+#include <utility>
+
+/**
+ * @title Rolling hash monoid
+ * @docs rolling_hash.md
+ */
+template <size_t N>
+struct RollingHashMonoid{
+  static std::array<int64_t, N> base;
+  static int64_t mod;
+
+  using value_type = std::pair<std::array<int64_t, N>, std::array<int64_t, N>>;
+
+  value_type id() const {
+    value_type ret;
+    for(size_t i = 0; i < N; ++i){
+      ret.first[i] = 0LL;
+      ret.second[i] = 1LL;
+    }
+    return ret;
+  }
+
+  value_type op(const value_type &a, const value_type &b) const {
+    value_type ret;
+    for(size_t i = 0; i < N; ++i){
+      ret.first[i] = (a.first[i] + b.first[i] * a.second[i] % mod) % mod;
+      ret.second[i] = a.second[i] * b.second[i] % mod;
+    }
+    return ret;
+  }
+
+  template <typename T>
+  static value_type make(T a){
+    value_type ret;
+    for(size_t i = 0; i < N; ++i){
+      ret.first[i] = (int64_t)a % mod;
+      ret.second[i] = base[i];
+    }
+    return ret;
+  }
+};
+
+template <size_t N> std::array<int64_t, N> RollingHashMonoid<N>::base;
+template <size_t N> int64_t RollingHashMonoid<N>::mod;
---
+++ test/aoj/2444/main.test.cpp
@@ -0,0 +1,41 @@
+#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=2444"
+
+#include <iostream>
+#include <string>
+#include <set>
+#include <array>
+
+#include "Mylib/IO/input_tuples.cpp"
+#include "Mylib/AlgebraicStructure/Monoid/rolling_hash.cpp"
+#include "Mylib/DataStructure/SegmentTree/segment_tree.cpp"
+
+int main(){
+  using Monoid = RollingHashMonoid<2>;
+  Monoid::base = {33, 100};
+  Monoid::mod = 1000000007;
+  
+  int n, m; std::cin >> n >> m;
+  std::string s; std::cin >> s;
+
+  SegmentTree<Monoid> seg(n);
+  for(int i = 0; i < n; ++i) seg.update(i, Monoid::make(s[i]));
+
+  std::set<std::array<int64_t, 2>> set;
+  int l = 0, r = 1;
+
+  for(auto [q] : input_tuples<std::string>(m)){
+    if(q[0] == 'L'){
+      if(q[1] == '+') ++l;
+      else --l;
+    }else{
+      if(q[1] == '+') ++r;
+      else --r;
+    }
+      
+    set.insert(seg.get(l, r).first);
+  }
+
+  std::cout << set.size() << "\n";
+
+  return 0;
+}
