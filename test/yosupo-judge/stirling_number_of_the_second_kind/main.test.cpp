d7cef831ba4d9eec3043faadaf9f2ff5dd63812b
Haar <haar@haar-pc>
2020-08-19 18:49:32 +0900

    [new] stirling_number_second_fft.cpp

--- test/yosupo-judge/stirling_number_of_the_second_kind/main.test.cpp
+++ test/yosupo-judge/stirling_number_of_the_second_kind/main.test.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include "Mylib/Convolution/ntt_convolution.cpp"
 #include "Mylib/Number/Mint/mint.cpp"
+#include "Mylib/Combinatorics/stirling_number_second_fft.cpp"
 #include "Mylib/IO/join.cpp"
 
 const int MOD = 998244353;
@@ -17,41 +18,11 @@ int main(){
 
   int N; std::cin >> N;
 
-  std::vector<mint> a(N+1), b(N+1);
+  auto ntt = NumberTheoreticTransform<mint,PRIM,1<<20>();
+  auto convolve = [&](const auto &a, const auto &b){return ntt.run_convolution(a, b);};
+  auto res = stirling_number_of_second_kind<mint>(N, convolve);
 
-  std::vector<int> m(N+1, 0);
-  for(int i = 2; i <= N; ++i){
-    if(m[i] != 0) continue;
-    for(int j = 2*i; j <= N; j += i){
-      m[j] = i;
-    }
-  }
+  std::cout << join(res.begin(), res.end()) << "\n";
 
-  for(int i = 0; i <= N; ++i){
-    if(m[i] == 0){
-      a[i] = mint::power(i, N);
-    }else{
-      a[i] = a[m[i]] * a[i/m[i]];
-    }
-  }
-    
-  mint f = 1;
-  for(int i = 1; i <= N; ++i) f *= i;
-  f = f.inv();
-
-  for(int i = N; i >= 0; --i){
-    a[i] *= f;
-    b[i] = f;
-    f *= i;
-
-    if(i % 2 == 1){
-      b[i] = -b[i];
-    }
-  }
-  
-  auto res = NumberTheoreticTransform<mint,PRIM,1<<20>().run_convolution(a, b);
-
-  std::cout << join(res.begin(), res.begin() + N + 1) << "\n";
-  
   return 0;
 }
