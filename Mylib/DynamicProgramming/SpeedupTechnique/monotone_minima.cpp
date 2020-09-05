9ecd49188b2724eb912d216f7dbab1760d34bc0c
Haar <haar@haar-pc>
2020-09-06 03:09:35 +0900

    [new] monotone minima

---
+++ Mylib/DynamicProgramming/SpeedupTechnique/monotone_minima.cpp
@@ -0,0 +1,47 @@
+#pragma once
+#include <vector>
+#include <utility>
+
+/**
+ * @title Monotone minima
+ * @docs monotone_minima.md
+ */
+namespace monotone_minima_impl {
+  template <typename T, typename F>
+  auto rec(
+    int N, int M, const F &f, int top, int bottom, int left, int right,
+    std::vector<std::pair<int, T>> &ret
+  ){
+    if(top > bottom) return;
+    if(top == bottom){
+      const int i = top;
+
+      int index = left;
+      T m = f(i, index);
+
+      for(int j = left; j <= right; ++j){
+        const auto temp = f(i, j);
+        if(temp < m){
+          m = temp;
+          index = j;
+        }
+      }
+
+      ret[i] = std::make_pair(index, m);
+      return;
+    }
+
+    const int mid = (top + bottom) / 2;
+    rec(N, M, f, mid, mid, left, right, ret);
+    rec(N, M, f, top, mid - 1, left, ret[mid].first, ret);
+    rec(N, M, f, mid + 1, bottom, ret[mid].first, right, ret);
+  }
+}
+
+template <typename T, typename F>
+auto monotone_minima(int N, int M, const F &f){
+  std::vector<std::pair<int, T>> ret(M);
+  monotone_minima_impl::rec<T, F>(N, M, f, 0, N - 1, 0, M - 1, ret);
+
+  return ret;
+}
---
+++ Mylib/DynamicProgramming/SpeedupTechnique/monotone_minima.md
@@ -0,0 +1,15 @@
+## Operations
+
+## Requirements
+
+## Notes
+
+## Problems
+
+- [C - スペースエクスプローラー高橋君](https://atcoder.jp/contests/colopl2018-final-open/tasks/colopl2018_final_c)
+
+## References
+
+- [https://ei1333.github.io/luzhiled/snippets/dp/monotone-minima.html](https://ei1333.github.io/luzhiled/snippets/dp/monotone-minima.html)
+- [https://ferin-tech.hatenablog.com/entry/2018/02/23/071343](https://ferin-tech.hatenablog.com/entry/2018/02/23/071343)
+- [https://wiki.kimiyuki.net/monotone%20minima](https://wiki.kimiyuki.net/monotone%20minima)
