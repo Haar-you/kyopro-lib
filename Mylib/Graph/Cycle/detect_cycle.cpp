1a39e23cbba52fc68eabcb13eff5aa602be73963
Haar <haar@haar-pc>
2020-09-10 06:29:03 +0900

    [modify] separate imlementations

--- Mylib/Graph/Cycle/detect_cycle.cpp
+++ Mylib/Graph/Cycle/detect_cycle.cpp
@@ -9,46 +9,54 @@
  * @docs detect_cycle.md
  */
 namespace haar_lib {
+  namespace detect_cycle_impl {
+    constexpr static int SEARCHED = 1;
+    constexpr static int SEARCHING = 2;
+
+    template <typename T>
+    std::optional<int> rec(
+      const graph<T> &g,
+      int cur,
+      std::vector<edge<T>> &temp,
+      std::vector<edge<T>> &ret,
+      std::vector<int> &check
+    ){
+      if(check[cur] == SEARCHED) return std::nullopt;
+      if(check[cur] == SEARCHING) return {cur};
+      check[cur] = SEARCHING;
+
+      for(auto &e : g[cur]){
+        temp.push_back(e);
+
+        if(auto res = rec(g, e.to, temp, ret, check); res){
+          if(*res != -1){
+            ret.push_back(e);
+            if(*res == cur){
+              return {-1};
+            }
+          }
+
+          return res;
+        }
+
+        temp.pop_back();
+      }
+
+      check[cur] = SEARCHED;
+
+      return std::nullopt;
+    }
+  }
+
   template <typename T>
   std::optional<std::vector<edge<T>>> detect_cycle(const graph<T> &g){
     const int N = g.size();
     std::vector<int> check(N);
 
-    constexpr int SEARCHED = 1;
-    constexpr int SEARCHING = 2;
-
-    auto rec =
-      [&](auto &rec, int cur, std::vector<edge<T>> &temp, std::vector<edge<T>> &ret) -> std::optional<int> {
-                                                                                                            if(check[cur] == SEARCHED) return std::nullopt;
-                                                                                                            if(check[cur] == SEARCHING) return {cur};
-                                                                                                            check[cur] = SEARCHING;
-
-                                                                                                            for(auto &e : g[cur]){
-                                                                                                              temp.push_back(e);
-
-                                                                                                              if(auto res = rec(rec, e.to, temp, ret); res){
-                                                                                                                if(*res != -1){
-                                                                                                                  ret.push_back(e);
-                                                                                                                  if(*res == cur){
-                                                                                                                    return {-1};
-                                                                                                                  }
-                                                                                                                }
-
-                                                                                                                return res;
-                                                                                                              }
-
-                                                                                                              temp.pop_back();
-                                                                                                            }
-
-                                                                                                            check[cur] = SEARCHED;
-
-                                                                                                            return std::nullopt;
-    };
-
     for(int i = 0; i < N; ++i){
       if(check[i] == 0){
         std::vector<edge<T>> temp, ret;
-        rec(rec, i, temp, ret);
+        detect_cycle_impl::rec(g, i, temp, ret, check);
         if(not ret.empty()){
           std::reverse(ret.begin(), ret.end());
           return {ret};
--- Mylib/Graph/Cycle/enumerate_functional_cycles.cpp
+++ Mylib/Graph/Cycle/enumerate_functional_cycles.cpp
@@ -8,44 +8,51 @@
  * @docs enumerate_functional_cycles.md
  */
 namespace haar_lib {
+  namespace enumerate_functional_cycles_impl {
+    constexpr static int SEARCHED = 1;
+    constexpr static int SEARCHING = 2;
+
+    std::optional<int> rec(
+      const std::vector<int> &g,
+      int cur,
+      std::vector<int> &temp,
+      std::vector<std::vector<int>> &ret,
+      std::vector<int> &check
+    ){
+      if(check[cur] == SEARCHED) return std::nullopt;
+      if(check[cur] == SEARCHING) return {cur};
+      check[cur] = SEARCHING;
+
+      const int i = g[cur];
+
+      if(auto res = rec(g, i, temp, ret, check); res){
+        if(*res != -1){
+          temp.push_back(i);
+          if(*res == cur){
+            check[cur] = SEARCHED;
+            return {-1};
+          }
+        }
+
+        check[cur] = SEARCHED;
+        return res;
+      }
+
+      check[cur] = SEARCHED;
+      return std::nullopt;
+    }
+  }
+
   std::vector<std::vector<int>> enumerate_functional_cycles(std::vector<int> g){
     const int n = g.size();
 
     std::vector<std::vector<int>> ret;
     std::vector<int> check(n);
 
-    constexpr int SEARCHED = 1;
-    constexpr int SEARCHING = 2;
-
-    auto rec =
-      [&](auto &rec, int cur, std::vector<int> &temp) -> std::optional<int> {
-                                                                             if(check[cur] == SEARCHED) return std::nullopt;
-                                                                             if(check[cur] == SEARCHING) return {cur};
-                                                                             check[cur] = SEARCHING;
-
-                                                                             const int i = g[cur];
-
-                                                                             if(auto res = rec(rec, i, temp); res){
-                                                                               if(*res != -1){
-                                                                                 temp.push_back(i);
-                                                                                 if(*res == cur){
-                                                                                   check[cur] = SEARCHED;
-                                                                                   return {-1};
-                                                                                 }
-                                                                               }
-
-                                                                               check[cur] = SEARCHED;
-                                                                               return res;
-                                                                             }
-
-                                                                             check[cur] = SEARCHED;
-                                                                             return std::nullopt;
-    };
-
     for(int i = 0; i < n; ++i){
       if(check[i] == 0){
         std::vector<int> temp;
-        auto res = rec(rec, i, temp);
+        auto res = enumerate_functional_cycles_impl::rec(g, i, temp, ret, check);
         if(res){
           std::reverse(temp.begin(), temp.end());
           ret.push_back(temp);
