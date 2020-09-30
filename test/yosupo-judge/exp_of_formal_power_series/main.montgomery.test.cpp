4aeaf4c91a7f71ca17ab2d418d1fe7e3c0be9c03
Haar <haar@haar-pc>
2020-10-01 05:15:44 +0900

    [new] add montgomery mint test

---
+++ test/yosupo-judge/exp_of_formal_power_series/main.montgomery.test.cpp
@@ -0,0 +1,33 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/exp_of_formal_power_series"
+
+#include <iostream>
+#include <vector>
+#include <functional>
+#include "Mylib/Number/Mint/montgomery.cpp"
+#include "Mylib/Convolution/ntt_convolution.cpp"
+#include "Mylib/Math/formal_power_series.cpp"
+#include "Mylib/IO/input_vector.cpp"
+#include "Mylib/IO/join.cpp"
+
+namespace hl = haar_lib;
+
+using mint = hl::montgomery<998244353>;
+using FPS = hl::formal_power_series<mint>;
+using NTT = hl::number_theoretic_transform<mint, 3, 1 << 21>;
+
+int main(){
+  using namespace std::placeholders;
+  std::cin.tie(0);
+  std::ios::sync_with_stdio(false);
+
+  auto ntt = NTT();
+  FPS::convolve = std::bind(&NTT::convolve<mint>, &ntt, _1, _2);
+
+  int N; std::cin >> N;
+  auto a = hl::input_vector<mint>(N);
+  auto ans = FPS(a).exp();
+
+  std::cout << hl::join(ans.begin(), ans.begin() + N) << "\n";
+
+  return 0;
+}
