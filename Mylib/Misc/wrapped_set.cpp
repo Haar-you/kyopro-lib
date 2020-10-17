210b74cf8466101094e4a3a14b9a64c600e2bb32
Haar <haar@haar-pc>
2020-10-18 05:36:21 +0900

    [modify] add contains, erase_if

--- Mylib/Misc/wrapped_set.cpp
+++ Mylib/Misc/wrapped_set.cpp
@@ -2,18 +2,32 @@
 #include <set>
 #include <functional>
 
-template <typename T, typename Compare = std::less<T>>
-class wrapped_set : public std::set<T, Compare> {
-  using super = std::set<T, Compare>;
+namespace haar_lib {
+  template <typename T, typename Compare = std::less<T>>
+  class wrapped_set : public std::set<T, Compare> {
+    using super = std::set<T, Compare>;
 
-public:
-  const T& max() const {
-    assert(not super::empty());
-    return *super::rbegin();
-  }
+  public:
+    const T& max() const {
+      assert(not super::empty());
+      return *super::rbegin();
+    }
 
-  const T& min() const {
-    assert(not super::empty());
-    return *super::begin();
-  }
-};
+    const T& min() const {
+      assert(not super::empty());
+      return *super::begin();
+    }
+
+    bool contains(const T &x) const {
+      return super::find(x) != super::end();
+    }
+
+    template <typename Predicate>
+    void erase_if(Predicate pred){
+      for(auto it = super::begin(); it != super::end();){
+        if(pred(*it)) it = super::erase(it);
+        else ++it;
+      }
+    }
+  };
+}
