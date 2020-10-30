ccd8b1aeeee00fc8b2dcb189f122dffecd71ad68
Haar <haar@haar-pc>
2020-10-30 23:28:24 +0900

    [change] add assert

--- Mylib/Convolution/fast_zeta_transform_subset.cpp
+++ Mylib/Convolution/fast_zeta_transform_subset.cpp
@@ -1,13 +1,16 @@
 #pragma once
 #include <vector>
 #include <functional>
+#include <cassert>
 
 namespace haar_lib {
   template <typename T, typename Func = std::plus<T>>
   std::vector<T> fast_zeta_transform_subset(std::vector<T> f, const Func &op = std::plus<T>()){
-    for(int i = 0; (1 << i) < (int)f.size(); ++i){
-      for(int j = 0; j < (int)f.size(); ++j){
-        if(j & (1 << i)) f[j] = op(f[j], f[j ^ (1 << i)]);
+    const int N = f.size();
+    assert((N & (N - 1)) == 0 && "N must be a power of 2");
+    for(int i = 1; i < N; i <<= 1){
+      for(int j = 0; j < N; ++j){
+        if(j & i) f[j] = op(f[j], f[j ^ i]);
       }
     }
     return f;
