0d84624b63f1d01f06cc8321596dfea89f6b6c0b
Haar <haar@haar-pc>
2020-10-31 12:39:43 +0900

    [new] convolution_gcd

---
+++ Mylib/Convolution/convolution_gcd.cpp
@@ -0,0 +1,51 @@
+#pragma once
+#include <cassert>
+#include <vector>
+
+namespace haar_lib {
+  template <typename T>
+  auto divisor_zeta_transform(std::vector<T> f){
+    const int N = f.size();
+    std::vector<bool> check(N, true);
+    for(int i = 2; i < N; ++i){
+      if(check[i]){
+        for(int j = (N - 1) / i; j > 0; --j){
+          check[j * i] = false;
+          f[j] += f[j * i];
+        }
+      }
+    }
+    return f;
+  }
+
+  template <typename T>
+  auto divisor_mobius_transform(std::vector<T> f){
+    const int N = f.size();
+    std::vector<bool> check(N, true);
+    for(int i = 2; i < N; ++i){
+      if(check[i]){
+        for(int j = 1; j * i < N; ++j){
+          check[j * i] = false;
+          f[j] -= f[j * i];
+        }
+      }
+    }
+    return f;
+  }
+
+  template <typename T>
+  auto convolution_gcd(std::vector<T> f, std::vector<T> g){
+    const int N = f.size();
+    assert(f.size() == g.size());
+
+    std::vector<T> ret(N);
+    for(int i = 1; i < N; ++i) ret[i] += f[i] + g[i];
+
+    f = divisor_zeta_transform(f);
+    g = divisor_zeta_transform(g);
+    for(int i = 0; i < N; ++i) f[i] *= g[i];
+    f = divisor_mobius_transform(f);
+    for(int i = 0; i < N; ++i) ret[i] += f[i];
+    return ret;
+  }
+}
---
+++ Mylib/Convolution/convolution_gcd.md
@@ -0,0 +1,20 @@
+---
+title: Convolution (Index GCD)
+documentation_of: Mylib/Convolution/convolution_gcd.cpp
+---
+
+## Operations
+
+## Requirements
+
+## Notes
+
+## Problems
+
+- [GCD Sum](https://www.codechef.com/problems/GCDSUM/)
+
+## References
+
+- [https://kazuma8128.hatenablog.com/entry/2018/07/29/231819](https://kazuma8128.hatenablog.com/entry/2018/07/29/231819)
+- [https://noshi91.hatenablog.com/entry/2018/12/27/121649](https://noshi91.hatenablog.com/entry/2018/12/27/121649)
+- [https://aprilganmo.hatenablog.com/entry/2020/07/24/190816](https://aprilganmo.hatenablog.com/entry/2020/07/24/190816)
