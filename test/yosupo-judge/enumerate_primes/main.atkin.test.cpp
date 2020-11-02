109302293e09856d22c3e375346f0e33e28e79ab
Haar <haar@haar-pc>
2020-11-02 13:08:20 +0900

    [new] enumerate_primes

---
+++ test/yosupo-judge/enumerate_primes/main.atkin.test.cpp
@@ -0,0 +1,34 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/enumerate_primes"
+
+#include <iostream>
+#include <vector>
+#include "Mylib/IO/join.cpp"
+#include "Mylib/Number/Prime/sieve_atkin.cpp"
+
+namespace hl = haar_lib;
+
+int main(){
+  std::cin.tie(0);
+  std::ios::sync_with_stdio(false);
+
+  int N, A, B; std::cin >> N >> A >> B;
+
+  const auto is_prime = hl::atkin_sieve(N);
+
+  int pi = 0;
+  std::vector<int> ans;
+
+  for(int i = 2; i <= N; ++i){
+    if(is_prime(i)){
+      if((pi - B) % A == 0){
+        ans.push_back(i);
+      }
+      ++pi;
+    }
+  }
+
+  std::cout << pi << " " << ans.size() << "\n"
+            << hl::join(ans.begin(), ans.end()) << "\n";
+
+  return 0;
+}
