9b0aac4fe712d6ea2ff02b6e74ac871158d7fb65
Haar <haar@haar-pc>
2020-12-27 03:37:49 +0900

    [new] enumerate mod inv

---
+++ Mylib/Number/Mod/enumerate_mod_inv.cpp
@@ -0,0 +1,14 @@
+#pragma once
+#include <vector>
+
+std::vector<int64_t> enumerate_mod_inv(int n, int64_t p){
+  std::vector<int64_t> ret(n + 1);
+
+  ret[1] = 1;
+
+  for(int i = 2; i <= n; ++i){
+    ret[i] = (p / i) * (p - ret[p % i]) % p;
+  }
+
+  return ret;
+}
---
+++ Mylib/Number/Mod/enumerate_mod_inv.md
@@ -0,0 +1,18 @@
+---
+title: Enumerate mod inv
+documentation_of: ./enumerate_mod_inv.cpp
+---
+
+## Operations
+
+- `enumerate_mod_inv(n, p)`
+  - `p` is prime
+  - Time complexity $O(n)$
+
+## Requirements
+
+## Notes
+
+## Problems
+
+## References
