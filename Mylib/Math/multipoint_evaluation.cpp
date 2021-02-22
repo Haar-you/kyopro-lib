702f3ff053c5a91947407660b4b8cdf7d996bd92
Haar <haar@haar-pc>
2021-02-22 18:04:12 +0900

    [update] multipoint_evaluation

--- Mylib/Math/multipoint_evaluation.cpp
+++ Mylib/Math/multipoint_evaluation.cpp
@@ -15,9 +15,9 @@ namespace haar_lib {
     for(int i = 0; i < M; ++i) f[i + k] = {-p[i], 1};
     for(int i = k - 1; i >= 1; --i) f[i] = f[i << 1 | 0] * f[i << 1 | 1];
 
-    f[1] = a.divmod(f[1]).second;
+    f[1] = a % f[1];
 
-    for(int i = 2; i < k + M; ++i) f[i] = f[i >> 1].divmod(f[i]).second;
+    for(int i = 2; i < k + M; ++i) f[i] = f[i >> 1] % f[i];
     for(int i = 0; i < M; ++i) ret[i] = f[k + i][0];
 
     return ret;
