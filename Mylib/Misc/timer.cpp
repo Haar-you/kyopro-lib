fe49ea7e2c60ff077214b0a49fa524e62a8c395c
Haar <haar@haar-pc>
2021-02-23 20:51:33 +0900

    [fix] add namespace

--- Mylib/Misc/timer.cpp
+++ Mylib/Misc/timer.cpp
@@ -2,20 +2,24 @@
 #include <chrono>
 #include <iostream>
 
-class timer {
-  std::chrono::system_clock::time_point s_, last_;
+namespace haar_lib {
+  class timer {
+    std::chrono::system_clock::time_point s_, last_;
 
-public:
-  timer(){}
+  public:
+    timer(){}
 
-  void set(){
-    s_ = last_ = std::chrono::system_clock::now();
-  }
+    void set(){
+      s_ = last_ = std::chrono::system_clock::now();
+    }
 
-  void time(){
-    auto t = std::chrono::system_clock::now();
-    auto d = std::chrono::duration_cast<std::chrono::milliseconds>(t - last_).count();
-    std::cerr << "\e[1;33m" << d << "ms\e[m\n";
-    last_ = t;
-  }
-};
+    void time(){
+#ifdef DEBUG
+      auto t = std::chrono::system_clock::now();
+      auto d = std::chrono::duration_cast<std::chrono::milliseconds>(t - last_).count();
+      std::cerr << "\e[1;33m" << d << "ms\e[m\n";
+      last_ = t;
+#endif
+    }
+  };
+}
