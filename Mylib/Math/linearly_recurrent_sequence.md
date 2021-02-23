d4829508c207039337b647cc09ec2eb9a07b14e9
Haar <haar@haar-pc>
2021-02-23 19:33:34 +0900

    [new] kth_term_of_linearly_recurrent_sequence

---
+++ Mylib/Math/linearly_recurrent_sequence.md
@@ -0,0 +1,18 @@
+---
+title: Kth term of linearly recurrent sequence
+documentation_of: ./linearly_recurrent_sequence.cpp
+---
+
+## Operations
+
+$a_i \equiv \sum_{j=0}^{d - 1}c_j a_{i - d + j}$を満たす数列aの$a_0 \ldots a_{d-1}$と係数$c_0 \ldots c_{d-1}$から$a_n$を求める。
+
+## Requirements
+
+## Notes
+
+## Problems
+
+## References
+
+- [https://arxiv.org/abs/2008.08822](https://arxiv.org/abs/2008.08822)
---
+++ test/yosupo-judge/kth_term_of_linearly_recurrent_sequence/main.test.cpp
@@ -0,0 +1,34 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/kth_term_of_linearly_recurrent_sequence"
+
+#include <iostream>
+#include "Mylib/Number/Mint/mint.cpp"
+#include "Mylib/Number/Prime/primitive_root.cpp"
+#include "Mylib/Convolution/ntt_convolution.cpp"
+#include "Mylib/Math/linearly_recurrent_sequence.cpp"
+#include "Mylib/IO/input_vector.cpp"
+
+namespace hl = haar_lib;
+
+constexpr int mod = 998244353;
+constexpr int prim_root = hl::primitive_root(mod);
+using mint = hl::modint<mod>;
+using NTT = hl::number_theoretic_transform<mint, prim_root, 1 << 21>;
+const static auto ntt = NTT();
+
+int main(){
+  std::cin.tie(0);
+  std::ios::sync_with_stdio(false);
+
+  int d; std::cin >> d;
+  int64_t k; std::cin >> k;
+
+  auto a = hl::input_vector<mint>(d);
+  auto c = hl::input_vector<mint>(d);
+  std::reverse(c.begin(), c.end());
+
+  auto ans = hl::linearly_recurrent_sequence<mint, ntt>(a, c, k);
+
+  std::cout << ans << "\n";
+
+  return 0;
+}
