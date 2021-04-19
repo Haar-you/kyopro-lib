16399ad275f59b6e22414c07f1631fab965284c8
Haar <haar@haar-pc>
2021-04-20 01:19:33 +0900

    [new] berlekamp_massey

---
+++ Mylib/Math/berlekamp_massey.md
@@ -0,0 +1,16 @@
+---
+title: Berlekamp-Massey
+documentation_of: ./berlekamp_massey.cpp
+---
+
+## Operations
+
+## Requirements
+
+## Notes
+
+## Problems
+
+## References
+
+- [https://en.wikipedia.org/wiki/Berlekamp%E2%80%93Massey_algorithm](https://en.wikipedia.org/wiki/Berlekamp%E2%80%93Massey_algorithm)
---
+++ test/yosupo-judge/find_linear_recurrence/main.test.cpp
@@ -0,0 +1,25 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/find_linear_recurrence"
+
+#include "Mylib/Number/Mint/mint.cpp"
+#include "Mylib/IO/input_vector.cpp"
+#include "Mylib/IO/join.cpp"
+#include "Mylib/Math/berlekamp_massey.cpp"
+
+namespace hl = haar_lib;
+
+constexpr int mod = 998244353;
+using mint = hl::modint<mod>;
+
+int main(){
+  std::cin.tie(0);
+  std::ios::sync_with_stdio(false);
+
+  int N; std::cin >> N;
+  auto a = hl::input_vector<mint>(N);
+
+  auto c = berlekamp_massey(a);
+  std::cout << c.size() << "\n"
+            << hl::join(c.begin(), c.end()) << "\n";
+
+  return 0;
+}
